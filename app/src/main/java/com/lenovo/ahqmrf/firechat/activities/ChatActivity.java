fea10a9ed5de5322a23fa9aa596c57777a4444cb
ahqmrf <ahqmrf@gmail.com>
2017-02-23 19:40:27 +0600

    changed

--- app/src/main/java/com/lenovo/ahqmrf/firechat/activities/ChatActivity.java
+++ app/src/main/java/com/lenovo/ahqmrf/firechat/activities/ChatActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -115,6 +117,23 @@ public class ChatActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        msgText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
 
             }
         });
